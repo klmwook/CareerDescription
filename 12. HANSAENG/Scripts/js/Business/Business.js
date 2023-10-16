80096f143f4d7de68e1cc8fc345bc4206a425c88
klmwook <klmwook72@gmail.com>
2023-10-16 13:18:32 +0900

    HANSAENG Project

---
+++ 12. HANSAENG/Scripts/js/Business/Business.js
@@ -0,0 +1,20 @@
+﻿//메인 탭처리
+
+var _vIndex = $("input[name=Business_index]").val();
+
+$(function () {
+
+    if (_fnToNull(_vIndex) != "") {
+        if (_vIndex == "1") {
+            var offset = $(".item01").eq(0).offset();
+        }
+        else if (_vIndex == "2") {
+            var offset = $(".item02").eq(0).offset();
+        }
+        else if (_vIndex == "3") {
+            var offset = $(".item03").eq(0).offset();
+        }
+
+        $('html, body').animate({ scrollTop: offset.top - 30 }, 1);
+    }
+});
